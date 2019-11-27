1bc77efd37e6890bf379c30f3f6a3190be1a0e2a
Gummibeer <dev.gummibeer@gmail.com>
2019-11-27 11:01:20 +0100

    cs

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,7 @@ module.exports = {
     base: process.env.BASE || '/',
     title: 'Elbgoods Guidelines',
     head: [
-        ['link', { rel: "shortcut icon", href: "/favicon.ico"}],
+        ['link', {rel: "shortcut icon", href: "/favicon.ico"}],
     ],
     themeConfig: {
         sidebarDepth: 2,
@@ -10,8 +10,8 @@ module.exports = {
             {text: 'Home', link: '/'},
             {text: 'PHP', link: '/php/'},
             {text: 'JS', link: '/js/'},
-            {text: 'API', link: '/api/' },
-            { text: 'GitHub', link: 'https://github.com/Gummibeer/eg-guidelines'},
+            {text: 'API', link: '/api/'},
+            {text: 'GitHub', link: 'https://github.com/Gummibeer/eg-guidelines'},
         ],
         sidebar: {
             '/php/': [
