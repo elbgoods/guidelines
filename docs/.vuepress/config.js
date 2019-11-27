2535d4201f1f279fdb06ac363acfab0fdcb01861
Gummibeer <dev.gummibeer@gmail.com>
2019-11-27 11:00:27 +0100

    editorconfig

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,10 +4,10 @@ module.exports = {
     themeConfig: {
         sidebarDepth: 2,
         nav: [
-            { text: 'Home', link: '/' },
-            { text: 'PHP', link: '/php/' },
-            { text: 'JS', link: '/js/' },
-            { text: 'GitHub', link: 'https://github.com/Gummibeer/eg-guidelines' },
+            {text: 'Home', link: '/'},
+            {text: 'PHP', link: '/php/'},
+            {text: 'JS', link: '/js/'},
+            {text: 'GitHub', link: 'https://github.com/Gummibeer/eg-guidelines'},
         ],
         sidebar: {
             '/php/': [
